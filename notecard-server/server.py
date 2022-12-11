31e13c60572d65d0eed809016d4d596e9f2bc310
Alan Mitchell <tabb99@gmail.com>
2022-12-10 21:19:44 -0900

    Setup config file use for server.py.

--- notecard-server/server.py
+++ notecard-server/server.py
@@ -1,13 +1,31 @@
 import json
+import sys
+from pathlib import Path
+from importlib import import_module
 
 from bottle import run, request, post
 
 import blues
 
-nCard = blues.Notecard(
-    "hub project here",
-    'device idenfier'
-    )
+# First command line argument is required and must give a path to a settings file.
+# Path to config file
+config_file = Path(sys.argv[1])
+config_dir = config_file.parent
+if config_dir != '.':
+    sys.path.insert(0, config_dir)
+settings = import_module(config_file.stem)
+
+# determine parameters for Notecard constructor
+param = {
+    'product_uid': settings.NOTECARD_PRODUCT,
+    'sn_string': settings.NOTECARD_SN_STRING
+}
+if hasattr(settings ,'NOTECARD_PORT_NAME'):
+    param['port_name'] = settings.NOTECARD_PORT_NAME
+if hasattr(settings, 'NOTECARD_UPLOAD_PERIOD'):
+    param['upload_period'] = settings.NOTECARD_UPLOAD_PERIOD
+
+nCard = blues.Notecard(**param)
 
 @post('/minimon')
 def mini_data():
